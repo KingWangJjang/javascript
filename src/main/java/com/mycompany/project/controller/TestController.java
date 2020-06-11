00f896a07a1973a4d91d2ad4bc99e5804e0a3a85
COM <COM@COM-PC>
2020-06-11 15:37:41 +0900

    method7 추가

--- src/main/java/com/mycompany/project/controller/TestController.java
+++ src/main/java/com/mycompany/project/controller/TestController.java
@@ -21,12 +21,12 @@ public class TestController {
 	public void method5() {
 		
 	}
-	
-	public void method7() {
+
+	public void method6() {
 		
 	}
 
-	public void method6() {
+	public void method7() {
 		
 	}
 }
